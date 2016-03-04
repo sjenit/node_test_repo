328b7f928e7e5de92b4df74c9ed7a2f34d55d24b
Jenit shah <shahkk44@gmail.com>
2016-03-04 14:54:43 +0530

    minor

--- routes/index.js
+++ routes/index.js
@@ -2,18 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  /*var headers = JSON.stringify(req.headers);
-  var body = JSON.stringify(req.body);
-  var query = JSON.stringify(req.query);*/
-    /*var resp = {
-        date:Date(),
-        headers:headers,
-        body:body,
-        query:query
-    };
-
-  //res.json({date:Date(),headers:headers,body:body, query:query});*/
     if (req.query.access_token) {
+        console.log("<html>Hello From Jenit Shah, Access Token is </html> " + req.query.access_token);
         res.send("<html>Hello From Jenit Shah, Access Token is </html> " + req.query.access_token)
     }
     else {
