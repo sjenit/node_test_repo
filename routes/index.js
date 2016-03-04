9763c151047d0e56e041b61124fa33bf297717fa
Jenit shah <shahkk44@gmail.com>
2016-03-04 13:02:49 +0530

    minor

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
-
 router.get('/', function(req, res, next) {
   var headers = JSON.stringify(req.headers);
   var body = JSON.stringify(req.body);
   var query = JSON.stringify(req.query);
     console.log({date:Date(),headers:headers,body:body, query:query});
-  res.json({date:Date(),headers:headers,body:body, query:query});
+  //res.json({date:Date(),headers:headers,body:body, query:query});
+    res.send("<html>Hello From Jenit Shah</html>")
 });
 
 router.post('/', function(req, res, next) {
