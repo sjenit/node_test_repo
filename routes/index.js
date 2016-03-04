b2e03709c8087b3a56e076f9a7ef7730989b0783
Jenit shah <shahkk44@gmail.com>
2016-03-04 14:29:15 +0530

    minor

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,7 @@ router.get('/', function(req, res, next) {
     console.log({date:Date(),headers:headers,body:body, query:query});
   //res.json({date:Date(),headers:headers,body:body, query:query});
     if (req.query.access_token) {
-        res.send("<html>Hello From Jenit Shah, Access Token is </html> " + req.query.access_token + )
+        res.send("<html>Hello From Jenit Shah, Access Token is </html> " + req.query.access_token)
     }
     else {
         res.send("<html>Hello From Jenit Shah, Code is " + req.query.code + " store hash is " + req.query.context + " scope is " + req.query.scope + "</html>");
