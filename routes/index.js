00afd5640b3dd4345d058a433b37bd393db6aaf5
Jenit shah <shahkk44@gmail.com>
2016-03-09 10:57:31 +0530

    minor

--- routes/index.js
+++ routes/index.js
@@ -2,42 +2,35 @@ var express = require('express');
 var request = require('request');
 var router = express.Router();
 
-router.get('/', function(req, res, next) {
-    console.log("new request...");
-    if (req.body.access_token) {
-        res.send("<html>Hello From Jenit Shah, Access Token is </html> " + req.body.access_token)
-    }
-    else {
-        res.send("<html>Hello From Jenit Shah, Code is " + req.query.code + " store hash is " + req.query.context + " scope is " + req.query.scope + "</html>");
-        /*var body = {
-            client_id:'a8dx216po0ikwmo19tchng8h4os09wc',
-            client_secret:'lhtw7adzr5m10zl9b0yb3j4bpqmk2c6',
-            code:'20hkbirv10wij2oqejafrknccd9iww4',
-            scope:'store_v2_products_read_only',
-            grant_type:'authorization_code',
-            redirect_uri:'https://jenitsimpleserver.herokuapp.com',
-            context:'stores/hb0qnk'
-        };
-        request.post("https://login.bigcommerce.com/oauth2/token", {
-                body: JSON.stringify(body),
-                headers: {
-                    'Content-Type': 'application/x-www-form-urlencoded'}
-            },
-            function (error, response, body) {
-                console.log(JSON.parse(body));
-                res.status(response.statusCode);
-                if (!error && response.statusCode == 200) {
-                    console.log(JSON.parse(body));
-                    res.json({status: 'success', response: JSON.parse(body)});
-                }
-                else {
-                    res.json({status: 'failure', error: 'Status Code:' + response.statusCode, error_desc:JSON.parse(body)});
-                }
-            });*/
-    }
-
-    //res.send("<html> req query="+JSON.parse(req.query)+" req body="+JSON.parse(req.body)+"</html>");
-
+router.get('/blogs', function(req, res, next) {
+    var blogs = [
+        {
+            title: 'Title of Demo 1',
+            body: 'Body of Demo 1',
+            Author: 'Author of Demo 1'
+        },
+        {
+            title: 'Title of Demo 2',
+            body: 'Body of Demo 2',
+            Author: 'Author of Demo 2'
+        },
+        {
+            title: 'Title of Demo 3',
+            body: 'Body of Demo 3',
+            Author: 'Author of Demo 3'
+        },
+        {
+            title: 'Title of Demo 4',
+            body: 'Body of Demo 4',
+            Author: 'Author of Demo 4'
+        },
+        {
+            title: 'Title of Demo 5',
+            body: 'Body of Demo 5',
+            Author: 'Author of Demo 5'
+        }
+    ];
+    res.json(blogs);
 });
 
 router.post('/', function(req, res, next) {
