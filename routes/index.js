456a28323b5fa478a70b06f1c0b62ab414e75725
Jenit shah <shahkk44@gmail.com>
2016-03-10 15:07:12 +0530

    minor

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,37 @@ router.get('/', function(req, res, next) {
     res.sendfile(__dirname+'/public/index.html');
 });
 
+router.get('/bigcomm', function(req, res) {
+    console.log("new request...");
+    if (req.body.access_token) {
+        res.send("<html>Hello From Jenit Shah, Access Token is </html> " + req.body.access_token)
+    }
+    else {
+        //res.send("<html>Hello From Jenit Shah, Code is " + req.query.code + " store hash is " + req.query.context + " scope is " + req.query.scope + "</html>");
+        var body = {
+            client_id:'a02uoc9lbd93uyl88vs9tmeobcfrdpa',
+            client_secret:'ghke5g3o1vfb6a9pf3dsp64ff6tbh2r',
+            code:req.query.code,
+            scope:'store_v2_products_read_only',
+            grant_type:'authorization_code',
+            redirect_uri:'https://jenitsimpleserver.herokuapp.com',
+            context:'stores/hb0qnk'
+        };
+        // request.post("https://login.bigcommerce.com/oauth2/token?client_id=a02uoc9lbd93uyl88vs9tmeobcfrdpa&client_secret=ghke5g3o1vfb6a9pf3dsp64ff6tbh2r&code=fae6ejhxiyxq8hpkss9vucxu4o3rmmn&scope=store_v2_products_read_only&grant_type=authorization_code&redirect_uri=https://jenitsimpleserver.herokuapp.com&context=stores/hb0qnk", {
+        request.post({url:'https://login.bigcommerce.com/oauth2/token?grant_type=authorization_code', form:JSON.stringify(body) }, function (error, response, body) {
+            console.log("here " + body);
+            res.status(response.statusCode);
+            if (!error && response.statusCode == 200) {
+                console.log(JSON.parse(body));
+                res.json({status: 'success', response: JSON.parse(body)});
+            }
+            else {
+                res.json({status: 'failure', error: 'Status Code:' + response.statusCode, error_desc:JSON.parse(body)});
+            }
+        });
+    }
+});
+
 router.post('/', function(req, res, next) {
     var headers = (req.headers);
     var body = (req.body);
